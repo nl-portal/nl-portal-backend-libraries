0feae628a3c29c68af69692cdddd406e8e3261a3
Anna Vissart <anna.vissart@ritense.com>
2023-04-21 17:37:04 +0200

    feat: github actions for validating builds
    * makes use of github actions to validate builds of commits and pull requests to the `development` branch
    * disables bootJar for module `:zgw:idtoken-authentication`

--- zgw/idtoken-authentication/build.gradle.kts
+++ zgw/idtoken-authentication/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
 val jar: Jar by tasks
 val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
-bootJar.enabled = true
+bootJar.enabled = false
 jar.enabled = true
 
 apply(from = "gradle/publishing.gradle")
