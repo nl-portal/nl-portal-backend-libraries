03e2a063fce75fce3056b3cce9c37d745b0e3ffa
Anna Vissart <anna.vissart@ritense.com>
2023-04-24 11:47:23 +0200

    chore: make test variables more obvious

--- zgw/idtoken-authentication/src/test/kotlin/com/ritense/portal/idtokenauthentication/service/IdTokenGeneratorTest.kt
+++ zgw/idtoken-authentication/src/test/kotlin/com/ritense/portal/idtokenauthentication/service/IdTokenGeneratorTest.kt
@@ -1,21 +1,31 @@
 package com.ritense.portal.idtokenauthentication.service
 
+import com.ritense.portal.idtokenauthentication.TestHelper.TEST_CLIENT_ID
+import com.ritense.portal.idtokenauthentication.TestHelper.TEST_ENCRYPTION_SECRET_INVALID
+import com.ritense.portal.idtokenauthentication.TestHelper.TEST_ENCRYPTION_SECRET_VALID
 import io.jsonwebtoken.Jwts
 import kotlin.test.assertEquals
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class IdTokenGeneratorTest {
     private val idTokenGenerator = IdTokenGenerator()
+    private lateinit var testClientId: String
+
+    @BeforeEach
+    fun prepareTest() {
+        testClientId = TEST_CLIENT_ID
+    }
 
     @Test
     fun `should generate token`() {
-        val testSecretKey = "NYy.TAQYbHRRWC_b2rfoqs4oR9jXj38N"
+        val testSecretKey = TEST_ENCRYPTION_SECRET_VALID
 
         val generatedToken = idTokenGenerator.generateToken(
             testSecretKey,
-            "testClientId"
+            testClientId
         )
 
         val claims = Jwts.parserBuilder()
@@ -23,20 +33,20 @@ class IdTokenGeneratorTest {
             .build()
             .parseClaimsJws(generatedToken)
 
-        Assertions.assertThat(claims.body.issuer).isEqualTo("testClientId")
-        Assertions.assertThat(claims.body.get("client_id")).isEqualTo("testClientId")
+        Assertions.assertThat(claims.body.issuer).isEqualTo(testClientId)
+        Assertions.assertThat(claims.body.get("client_id")).isEqualTo(testClientId)
         Assertions.assertThat(claims.body.get("user_id")).isEqualTo("Valtimo")
         Assertions.assertThat(claims.body.get("user_representation")).isEqualTo("")
     }
 
     @Test
     fun `should throw error when secret is too short`() {
-        val testSecretKey = "RRWC_b2rfXj38Noqs4o.R9j"
+        val testSecretKey = TEST_ENCRYPTION_SECRET_INVALID
 
         val exception = assertThrows(IllegalArgumentException::class.java) {
             idTokenGenerator.generateToken(
                 testSecretKey,
-                "testClientId"
+                testClientId
             )
         }
 
