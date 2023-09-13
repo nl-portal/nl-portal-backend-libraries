f8f73a103926c688de3751a71f13dc9500424a78
Frank van der Linden <flinden68@elstarit.nl>
2023-09-13 09:54:12 +0200

    fixed for issue with parsing JSON after upgrade

--- graphql/src/main/kotlin/com/ritense/portal/graphql/security/directive/CustomSpringDataFetcher.kt
+++ graphql/src/main/kotlin/com/ritense/portal/graphql/security/directive/CustomSpringDataFetcher.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ritense.portal.graphql.security.directive
 
 import com.expediagroup.graphql.generator.annotations.GraphQLName
@@ -23,44 +38,44 @@ import kotlin.reflect.jvm.javaType
 import kotlin.reflect.jvm.jvmErasure
 
 class CustomSpringDataFetcher(
-        private val target: Any?,
-        private val fn: KFunction<*>,
-        private val objectMapper: ObjectMapper = jacksonObjectMapper(),
-        private val applicationContext: ApplicationContext
-): FunctionDataFetcher(target, fn) {
+    private val target: Any?,
+    private val fn: KFunction<*>,
+    private val objectMapper: ObjectMapper = jacksonObjectMapper(),
+    private val applicationContext: ApplicationContext
+) : FunctionDataFetcher(target, fn) {
 
     override fun mapParameterToValue(param: KParameter, environment: DataFetchingEnvironment): Pair<KParameter, Any?>? =
-            if (param.hasAnnotation<Autowired>()) {
-                val qualifier = param.findAnnotation<Qualifier>()?.value
-                if (qualifier != null) {
-                    param to applicationContext.getBean(qualifier)
-                } else {
-                    param to applicationContext.getBean(param.type.javaType as Class<*>)
-                }
+        if (param.hasAnnotation<Autowired>()) {
+            val qualifier = param.findAnnotation<Qualifier>()?.value
+            if (qualifier != null) {
+                param to applicationContext.getBean(qualifier)
             } else {
-                when {
-                    param.isDataFetchingEnvironment() -> param to environment
-                    else -> {
-                        val name = param.getName()
-                        if (environment.containsArgument(name) || param.type.isOptionalInputType()) {
-                            val value: Any? = environment.arguments[name]
-                            param to convertArgumentToObject(param, environment, name, value)
-                        } else {
-                            null
-                        }
+                param to applicationContext.getBean(param.type.javaType as Class<*>)
+            }
+        } else {
+            when {
+                param.isDataFetchingEnvironment() -> param to environment
+                else -> {
+                    val name = param.getName()
+                    if (environment.containsArgument(name) || param.type.isOptionalInputType()) {
+                        val value: Any? = environment.arguments[name]
+                        param to convertArgumentToObject(param, environment, name, value)
+                    } else {
+                        null
                     }
                 }
             }
+        }
 
     /**
      * Convert the generic arument value from JSON input to the paramter class.
      * This is currently achieved by using a Jackson ObjectMapper.
      */
     private fun convertArgumentToObject(
-            param: KParameter,
-            environment: DataFetchingEnvironment,
-            argumentName: String,
-            argumentValue: Any?
+        param: KParameter,
+        environment: DataFetchingEnvironment,
+        argumentName: String,
+        argumentValue: Any?
     ): Any? = when {
         param.type.isOptionalInputType() -> {
             when {
@@ -77,8 +92,8 @@ class CustomSpringDataFetcher(
     }
 
     private fun convertValue(
-            paramType: KType,
-            argumentValue: Any?
+        paramType: KType,
+        argumentValue: Any?
     ): Any? = when {
         paramType.isList() -> {
             val argumentClass = paramType.getTypeOfFirstArgument().getJavaClass()
@@ -103,9 +118,10 @@ class CustomSpringDataFetcher(
     private fun KType.isSubclassOf(kClass: KClass<*>) = this.jvmErasure.isSubclassOf(kClass)
     private fun KType.getJavaClass(): Class<*> = this.jvmErasure.java
     private fun KParameter.getName(): String =
-            this.getGraphQLName() ?: this.name ?: throw CouldNotGetNameOfKParameterException(this)
+        this.getGraphQLName() ?: this.name ?: throw CouldNotGetNameOfKParameterException(this)
     private fun KAnnotatedElement.getGraphQLName(): String? = this.findAnnotation<GraphQLName>()?.value
+
     @Throws(InvalidWrappedTypeException::class)
     private fun KType.getTypeOfFirstArgument(): KType =
-            this.arguments.firstOrNull()?.type ?: throw InvalidWrappedTypeException(this)
+        this.arguments.firstOrNull()?.type ?: throw InvalidWrappedTypeException(this)
 }
