244c69fc1606f25c5f5a500a4f69073f198daa50
Peter van Manen <peter.van.manen@ritense.com>
2023-10-05 09:13:00 +0200

    publish to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,8 +131,8 @@ subprojects {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/nl-portal/nl-portal-backend-libraries")
                 credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+                    username = System.getenv("USER")
+                    password = System.getenv("TOKEN")
                 }
             }
         }
