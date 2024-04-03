8e6260d6d32d01c8499cc9c9c6d1139486a65406
niekmol <niek.mol@ritense.com>
2024-04-03 13:26:10 +0200

    ktlint build error fix

--- zgw/common-ground-authentication/src/main/kotlin/nl/nlportal/commonground/authentication/Keycloak.kt
+++ zgw/common-ground-authentication/src/main/kotlin/nl/nlportal/commonground/authentication/Keycloak.kt
@@ -5,9 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "keycloak")
 data class Keycloak(
     var resource: String = "",
-    var credentials: Credentials = Credentials("")
-    )
+    var credentials: Credentials = Credentials(""),
+)
 
 data class Credentials(
-    var secret: String = ""
+    var secret: String = "",
 )
