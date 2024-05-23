283e297366dbab8ae9f5aaae92d165485e1fa3da
Frank van der Linden <flinden68@elstarit.nl>
2024-05-23 12:19:59 +0200

    fix for DataBufferLimitException on OpenZaak

--- zgw/zaken-api/src/main/kotlin/nl/nlportal/zakenapi/client/ZakenApiClient.kt
+++ zgw/zaken-api/src/main/kotlin/nl/nlportal/zakenapi/client/ZakenApiClient.kt
@@ -25,6 +25,7 @@ import nl.nlportal.zakenapi.domain.ZaakStatus
 import io.netty.handler.logging.LogLevel
 import org.springframework.http.HttpStatus
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.server.ResponseStatusException
@@ -153,6 +154,10 @@ class ZakenApiClient(
     }
 
     private fun webClient(): WebClient {
+        val strategies =
+            ExchangeStrategies.builder()
+                .codecs { configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024) }
+                .build()
         val token =
             idTokenGenerator.generateToken(
                 zakenApiConfig.secret,
@@ -169,6 +174,7 @@ class ZakenApiClient(
                     ),
                 ),
             )
+            .exchangeStrategies(strategies)
             .baseUrl(zakenApiConfig.url)
             .defaultHeader("Accept-Crs", "EPSG:4326")
             .defaultHeader("Content-Crs", "EPSG:4326")
