4bac6edd43b1546547ed7ed2617a00a5929ef782
Frank van der Linden <flinden68@elstarit.nl>
2024-07-03 12:11:01 +0200

    feedback PR

--- zgw/taak/src/main/kotlin/nl/nlportal/zgw/taak/domain/TaakFormulierV2.kt
+++ zgw/taak/src/main/kotlin/nl/nlportal/zgw/taak/domain/TaakFormulierV2.kt
@@ -7,10 +7,11 @@ data class TaakFormulierV2(
     companion object {
         fun migrate(taakFormulier: TaakFormulier): TaakFormulierV2 {
             val value = taakFormulier.value
-            if (value.startsWith("http")) {
-                return TaakFormulierV2("url", value)
+            return if (value.startsWith("http")) {
+                TaakFormulierV2("url", value)
+            } else {
+                TaakFormulierV2("id", value)
             }
-            return TaakFormulierV2("id", value)
         }
     }
 }
