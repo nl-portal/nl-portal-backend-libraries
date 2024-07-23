1edb1f135f786de192c822bdd4498e229b5614e8
Anna Vissart <anna.vissart@ritense.com>
2024-07-23 20:10:32 +0200

    chore: spotless

--- zgw/berichten/src/main/kotlin/nl/nlportal/berichten/graphql/BerichtenQuery.kt
+++ zgw/berichten/src/main/kotlin/nl/nlportal/berichten/graphql/BerichtenQuery.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package nl.nlportal.berichten.graphql
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
