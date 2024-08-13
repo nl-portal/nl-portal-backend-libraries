6f8fe80296afd55696b58e2d436dee4f7cdfecdc
Anna <100125713+anna-ritense@users.noreply.github.com>
2024-08-13 13:30:23 +0200

    story: 117274 berichten queries (#279)
    * feat: 117270 inital berichten module
    
    * adds new zgw berichten module
    * adds initial configuration, properties and domain entities for the new berichten module
    
    * chore: dependency and ktlint
    
    * chore: copyright
    
    * chore: spotless check
    
    * feat: 117270 inital berichten module
    
    * adds new zgw berichten module
    * adds initial configuration, properties and domain entities for the new berichten module
    
    * chore: dependency and ktlint
    
    * chore: copyright
    
    * chore: spotless check
    
    * feat: update domain objects after discussions
    
    * feat: 117274 berichten queries
    
    * initial queries
    
    * chore: spotless
    
    * feat: implement graphql query for getting berichten
    
    * feat: integration test
    
    * feat: add single bericht query
    
    * feat: add/modify more queries
    
    * feat: don't return exceptions when bericht query fails
    
    * feat: additional wip tests
    
    * feat: wip tests
    
    * feat: adjust tests
    
    * feat: modify github actions
    
    * feat: only validate build on pull request
    
    * feat: validate on push to important branches
    
    * feat: fix typo, update query name

---
+++ zgw/berichten/src/test/kotlin/nl/nlportal/berichten/TestApplication.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.nlportal.berichten
+
+import nl.nlportal.core.security.OauthSecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@SpringBootApplication(
+    exclude = [
+        OauthSecurityAutoConfiguration::class,
+    ],
+)
+class TestApplication {
+    fun main(args: Array<String>) {
+        runApplication<TestApplication>(*args)
+    }
+
+    @Bean
+    fun springSecurityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeExchange {
+                it.anyExchange().permitAll()
+            }
+            .build()
+    }
+}
