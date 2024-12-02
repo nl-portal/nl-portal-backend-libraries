aaa199c43484c3709c109f648a49ee0e0f8180a9
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2024-12-02 15:08:15 +0100

    add pageSize to getZaken. It is optional to be backwards compatible

--- core/src/main/kotlin/nl/nlportal/core/autoconfiguration/CoreAutoConfiguration.kt
+++ core/src/main/kotlin/nl/nlportal/core/autoconfiguration/CoreAutoConfiguration.kt
@@ -17,9 +17,11 @@ package nl.nlportal.core.autoconfiguration
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import nl.nlportal.core.util.Mapper
+import nl.nlportal.core.util.ProfileNameManager
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 
 @AutoConfiguration
 class CoreAutoConfiguration {
@@ -27,5 +29,10 @@ class CoreAutoConfiguration {
     @ConditionalOnMissingBean(name = ["objectMapper"])
     fun objectMapper(): ObjectMapper {
         return Mapper.get()
+    }
+
+    @Bean
+    fun profileNameManager(environment: Environment): ProfileNameManager {
+        return ProfileNameManager(environment)
     }
 }
