65b94c57b0c8edfad1b1f75d6c50b91c2600de43
Anna Vissart <anna.vissart@ritense.com>
2025-02-06 17:26:11 +0100

    * update copyright

--- payment/src/main/kotlin/nl/nlportal/payment/service/OgonePaymentService.kt
+++ payment/src/main/kotlin/nl/nlportal/payment/service/OgonePaymentService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Ritense BV, the Netherlands.
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,7 +160,7 @@ class OgonePaymentService(
                         .append(field.value)
                         .append(shaKey)
                 }
-            logger.info("SHA version: {} - {}", shaVersion, parametersConcatenation.toString())
+            logger.info{"SHA version: ${shaVersion} - ${ parametersConcatenation}"}
             return CoreUtils.createHash(parametersConcatenation.toString(), shaVersion)
         }
     }
--- zgw/documenten-api/src/main/kotlin/nl/nlportal/documentenapi/autoconfigure/ClamAVConfiguration.kt
+++ zgw/documenten-api/src/main/kotlin/nl/nlportal/documentenapi/autoconfigure/ClamAVConfiguration.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package nl.nlportal.documentenapi.autoconfigure
 
 import io.github.oshai.kotlinlogging.KotlinLogging
@@ -22,7 +38,7 @@ class ClamAVConfiguration {
     @Bean
     @ConditionalOnMissingBean(VirusScanService::class)
     fun virusScanService(clamAVVirusScanConfig: ClamAVVirusScanConfig): VirusScanService {
-        logger.info("ClamAV virusscan is loaded with host: {} and port: {}", clamAVVirusScanConfig.hostName, clamAVVirusScanConfig.port)
+        logger.info { "ClamAV virusscan is loaded with host: ${clamAVVirusScanConfig.hostName} and port: ${clamAVVirusScanConfig.port}" }
         return ClamAVService(clamAVVirusScanConfig)
     }
 
