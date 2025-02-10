40c9febb794323e94cf6738f091dbbf950a3ddd5
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2025-02-10 16:20:34 +0100

    change in TaakKoppeling V2, uuid is deprecated and replaced by value. With backwards com

--- zgw/taak/src/main/kotlin/nl/nlportal/zgw/taak/domain/TaakKoppeling.kt
+++ zgw/taak/src/main/kotlin/nl/nlportal/zgw/taak/domain/TaakKoppeling.kt
@@ -1,23 +1,34 @@
 package nl.nlportal.zgw.taak.domain
 
+import com.expediagroup.graphql.generator.annotations.GraphQLIgnore
 import nl.nlportal.core.util.CoreUtils
 import java.util.UUID
 
 data class TaakKoppeling(
     val registratie: String,
-    val uuid: UUID?,
+    @GraphQLIgnore
+    val uuid: UUID? = null,
+    @GraphQLIgnore
+    val value: String? = null,
 ) {
+    fun value(): String? {
+        if (uuid != null) {
+            return uuid.toString()
+        }
+        return value
+    }
+
     companion object {
         fun migrate(zaak: String?): TaakKoppeling {
             return if (zaak != null) {
                 TaakKoppeling(
                     registratie = TaakKoppelingRegistratie.ZAAK.value,
                     uuid = CoreUtils.extractId(zaak),
+                    value = CoreUtils.extractId(zaak).toString(),
                 )
             } else {
                 TaakKoppeling(
                     registratie = TaakKoppelingRegistratie.ZAAK.value,
-                    uuid = null,
                 )
             }
         }
