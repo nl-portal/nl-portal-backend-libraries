b3009f2046520d10d50831020c9c4f07edec8ab7
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2025-06-12 15:47:31 +0200

    haalcentraal V2: implement officialLastName, because it was forgotten :-(

--- haalcentraal2/src/main/kotlin/nl/nlportal/haalcentraal2/domain/brp/BrpNaam.kt
+++ haalcentraal2/src/main/kotlin/nl/nlportal/haalcentraal2/domain/brp/BrpNaam.kt
@@ -15,6 +15,8 @@
  */
 package nl.nlportal.haalcentraal2.domain.brp
 
+import com.fasterxml.jackson.annotation.JsonValue
+
 data class BrpNaam(
     val voornamen: String? = null,
     val adellijkeTitelPredicaat: BrpAdellijkeTitelPredicaat? = null,
@@ -50,3 +52,12 @@ data class BrpNaamInOnderzoek(
     val aanduidingNaamgebruik: Boolean? = false,
     val volledigeNaam: Boolean? = false,
 )
+
+enum class AanduidingNaamGebruikBrpNaam(
+    @JsonValue val value: String,
+) {
+    EIGEN("E"),
+    EIGEN_PARTNER("N"),
+    PARTNER("P"),
+    PARTNER_EIGEN("V"),
+}
