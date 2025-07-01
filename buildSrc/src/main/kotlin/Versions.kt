b0c4c25d892bba88274234dcf7f01d2c0b3754c5
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2025-07-01 09:40:23 +0200

    merged TestVersions into Versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,7 +18,7 @@ object Versions {
     const val everitJsonSchema = "1.14.6"
     const val jsonPath = "2.9.0"
     const val jsonWebTokens = "0.12.6"
-    const val kotlinCoroutines = "1.8.1"
+    const val kotlinCoroutines = "1.10.1"
     const val apacheTikaVersion="3.1.0"
     const val clamAvVersion="2.1.2"
     const val apacheCommons = "3.17.0"
@@ -29,11 +29,9 @@ object Versions {
     const val jacksonBom = "2.18.2"
     const val kotlinLogging = "7.0.4"
     const val springCloud = "4.3.0"
-}
 
-internal object TestVersions {
+    //Test versions
     const val hamcrest = "3.0"
-    const val kotlinCoroutines = "1.10.1"
     const val mockitoKotlin = "5.4.0"
     const val okHttp3 = "4.12.0"
     const val postgresql = "42.7.5"
