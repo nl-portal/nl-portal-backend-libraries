b02c474c8314155fa5e4c986fe6a63db3a7fc89f
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2026-01-16 20:16:27 +1100

    feat: make verificatie client and service only available if verificatie.enabled = true. Removed unused code from tests

--- zgw/verificatie/src/test/kotlin/nl/nlportal/verificatie/graphql/VerificatieQueryIT.kt
+++ zgw/verificatie/src/test/kotlin/nl/nlportal/verificatie/graphql/VerificatieQueryIT.kt
@@ -19,12 +19,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import kotlinx.coroutines.test.runTest
 import nl.nlportal.commonground.authentication.WithBurgerUser
 import nl.nlportal.verificatie.TestHelper
-import nl.nlportal.verificatie.autoconfigure.VerificatieModuleConfiguration
-import okhttp3.mockwebserver.MockWebServer
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
@@ -32,8 +27,6 @@ import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureH
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.graphql.test.tester.HttpGraphQlTester
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 
 @SpringBootTest
 @AutoConfigureHttpGraphQlTester
@@ -41,42 +34,7 @@ import org.springframework.test.context.DynamicPropertySource
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class VerificatieQueryIT(
     @Autowired private val httpGraphQlTester: HttpGraphQlTester,
-    @Autowired private val verificatieModuleConfiguration: VerificatieModuleConfiguration,
 ) {
-    companion object {
-        @JvmStatic
-        var server: MockWebServer? = null
-
-        @JvmStatic
-        var url: String = ""
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun properties(propsRegistry: DynamicPropertyRegistry) {
-            propsRegistry.add("nl-portal.config.verificatie.enabled") { true }
-        }
-
-        @JvmStatic
-        @BeforeAll
-        fun beforeAll() {
-            server = MockWebServer()
-            server?.start()
-            url = server?.url("/").toString()
-        }
-
-        @JvmStatic
-        @AfterAll
-        fun afterAll() {
-            server?.shutdown()
-        }
-    }
-
-    @BeforeEach
-    internal fun setUp() {
-        url = server?.url("/").toString()
-        verificatieModuleConfiguration.enabled = true
-    }
-
     @Test
     @WithBurgerUser("569312863")
     fun `is verificatie Enabled`() =
