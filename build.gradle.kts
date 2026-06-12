ce010f7499c6be2ec8533eb9ef29899abb4ce6c0
Frank van der Linden | elstar IT <flinden68@users.noreply.github.com>
2026-06-12 13:34:47 +0200

    feat: revert to spring boot 4.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -200,8 +200,8 @@ listOf(":app", ":gradle:cve-report", ":gradle:license-report").forEach { moduleP
     }
 }
 
-//override hibernate version, which is compatible with Jackson 3.x
-//extra["hibernate.version"] = "7.3.5.Final"
+//TODO override hibernate version, which is compatible with Jackson 3.x. Is fixed in Spring 4.1.0
+extra["hibernate.version"] = "7.3.5.Final"
 
 tasks.register<HtmlDependencyReportTask>("htmlDependencyReport")
 
